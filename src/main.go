85499efbeff37fec667fbfdac3a5800fbd8ee4e7
jrvldam <jrvldam@gmail.com>
2021-12-12 01:33:58 +0100

    get access token endpoint

---
+++ src/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/jrvldam/bookstore_oauth-api/src/app"
+
+func main() {
+	app.StartApplication()
+}
---
+++ src/repository/db/db_repository.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+	"github.com/jrvldam/bookstore_oauth-api/src/domain/access_token"
+	"github.com/jrvldam/bookstore_oauth-api/src/utils/errors"
+)
+
+type DbRepository interface {
+	GetById(string) (*access_token.AccessToken, *errors.RestErr)
+}
+
+type dbRepository struct{}
+
+func NewRepository() DbRepository {
+	return &dbRepository{}
+}
+
+func (r *dbRepository) GetById(id string) (*access_token.AccessToken, *errors.RestErr) {
+	return nil, errors.NewInternalServerError("Database connection not implemented yet")
+}
